a8b19e6f4fa6b5a4330360b158291e80f0c3bb1f
Murilo Felpeto <murilo.gilfelpeto@wexinc.com>
2024-12-31 11:32:11 -0300

    added stack docker compose and metrics

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,20 +3,25 @@ package main
 import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"net/http"
 )
 
 func (app *application) routes() http.Handler {
 	mux := chi.NewRouter()
+	metricMiddleware := NewPatternMiddleware("webapp")
 
 	// register middleware
 	mux.Use(middleware.Recoverer)
+	mux.Use(metricMiddleware)
 	mux.Use(app.addIpToContext)
 	mux.Use(app.Session.LoadAndSave)
 
 	// register routes
 	mux.Get("/", app.Home)
 	mux.Post("/login", app.Login)
+
+	mux.Handle("/metrics/prometheus", promhttp.Handler())
 
 	// static assets
 	fileServer := http.FileServer(http.Dir("./static/"))
